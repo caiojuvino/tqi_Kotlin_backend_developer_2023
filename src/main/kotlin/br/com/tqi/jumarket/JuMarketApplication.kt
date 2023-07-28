2bc3b8b953626a9073dc1b793e5b56c99a1ebf9f
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-07-27 21:33:36 -0300

    Project setup

---
+++ src/main/kotlin/br/com/tqi/jumarket/JuMarketApplication.kt
@@ -0,0 +1,11 @@
+package br.com.tqi.jumarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JuMarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<JuMarketApplication>(*args)
+}
